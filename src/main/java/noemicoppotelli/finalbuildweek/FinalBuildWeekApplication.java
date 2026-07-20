1c89a803f601371492c45f70c998b179b0357541
Noemi Coppotelli <liz.c2004@gmail.com>
2026-07-20 15:19:16 +0200

    first commit

---
+++ src/main/java/noemicoppotelli/finalbuildweek/FinalBuildWeekApplication.java
@@ -0,0 +1,13 @@
+package noemicoppotelli.finalbuildweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalBuildWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalBuildWeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/noemicoppotelli/finalbuildweek/FinalBuildWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package noemicoppotelli.finalbuildweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalBuildWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
