0c8c0545c121ad6cb9b5e33f45507c0783aeabd2
cafagnarob <cafagna.rob@gmail.com>
2026-07-20 16:26:10 +0200

    Update Indirizzo.java

--- src/main/java/noemicoppotelli/finalbuildweek/entities/Indirizzo.java
+++ src/main/java/noemicoppotelli/finalbuildweek/entities/Indirizzo.java
@@ -14,19 +14,37 @@ public class Indirizzo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
     private Cliente cliente;
 
     @Column(nullable = false)
     private TipoIndirizzo tipoIndirizzo;
 
+    @Column(nullable = false)
     private String via;
 
+    @Column(nullable = false)
     private int civico;
 
+    @Column(nullable = false)
     private int cap;
 
     private String localita;
 
+    @ManyToOne
+    @JoinColumn(name = "comune_id")
     private Comune comune;
+
+
+    private Indirizzo(Cliente cliente, TipoIndirizzo tipoIndirizzo,
+                      String via, int civico, int cap, Comune comune) {
+        this.cliente = cliente;
+        this.tipoIndirizzo = tipoIndirizzo;
+        this.via = via;
+        this.civico = civico;
+        this.cap = cap;
+        this.localita = comune.getNome();
+        this.comune = comune;
+    }
 }
