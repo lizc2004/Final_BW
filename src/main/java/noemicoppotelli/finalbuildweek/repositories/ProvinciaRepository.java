d7f620ecae7b833876236ab6ddc0cef55b2d9766
cafagnarob <cafagna.rob@gmail.com>
2026-07-21 10:16:06 +0200

    Create ProvinciaRepository.java

---
+++ src/main/java/noemicoppotelli/finalbuildweek/repositories/ProvinciaRepository.java
@@ -0,0 +1,14 @@
+package noemicoppotelli.finalbuildweek.repositories;
+
+import noemicoppotelli.finalbuildweek.entities.Provincia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProvinciaRepository extends JpaRepository<Provincia, Long> {
+
+    Optional<Provincia> findByName(String name);
+
+    Optional<Provincia> findBySigla(String sigla);
+    
+}
