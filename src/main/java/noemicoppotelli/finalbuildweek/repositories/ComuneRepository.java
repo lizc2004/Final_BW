80cdcda3a45d401be277b15ea53b26e6cc0e3ab3
cafagnarob <cafagna.rob@gmail.com>
2026-07-21 11:08:52 +0200

    Update ComuneRepository.java

--- src/main/java/noemicoppotelli/finalbuildweek/repositories/ComuneRepository.java
+++ src/main/java/noemicoppotelli/finalbuildweek/repositories/ComuneRepository.java
@@ -4,6 +4,10 @@ import noemicoppotelli.finalbuildweek.entities.Comune;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ComuneRepository extends JpaRepository<Comune, Long> {
+
+    Optional<Comune> findByNome(String name);
 }
