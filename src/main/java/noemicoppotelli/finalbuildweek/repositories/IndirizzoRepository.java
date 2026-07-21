ec45918f95db8a61a610181dbcb2f996250cfb2c
cafagnarob <cafagna.rob@gmail.com>
2026-07-21 17:26:36 +0200

    Update IndirizzoRepository.java

--- src/main/java/noemicoppotelli/finalbuildweek/repositories/IndirizzoRepository.java
+++ src/main/java/noemicoppotelli/finalbuildweek/repositories/IndirizzoRepository.java
@@ -3,10 +3,12 @@ package noemicoppotelli.finalbuildweek.repositories;
 import noemicoppotelli.finalbuildweek.entities.Indirizzo;
 import noemicoppotelli.finalbuildweek.enums.TipoIndirizzo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface IndirizzoRepository extends JpaRepository<Indirizzo, Long> {
     List<Indirizzo> findByClienteId(Long clienteId);
 
