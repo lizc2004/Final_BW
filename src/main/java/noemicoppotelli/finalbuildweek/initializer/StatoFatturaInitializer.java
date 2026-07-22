710c02e6ab7f22072a373d73d9e83332a0a6d4a0
cafagnarob <cafagna.rob@gmail.com>
2026-07-22 15:51:31 +0200

    Create StatoFatturaInitializer.java

---
+++ src/main/java/noemicoppotelli/finalbuildweek/initializer/StatoFatturaInitializer.java
@@ -0,0 +1,27 @@
+package noemicoppotelli.finalbuildweek.initializer;
+
+import noemicoppotelli.finalbuildweek.entities.StatoFattura;
+import noemicoppotelli.finalbuildweek.repositories.StatoFatturaRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(4)
+public class StatoFatturaInitializer implements CommandLineRunner {
+    private final StatoFatturaRepository statoFatturaRepository;
+
+    public StatoFatturaInitializer(StatoFatturaRepository statoFatturaRepository) {
+        this.statoFatturaRepository = statoFatturaRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        if (statoFatturaRepository.count() > 0) {
+            return;
+        }
+        this.statoFatturaRepository.save(new StatoFattura("PAGATA"));
+        this.statoFatturaRepository.save(new StatoFattura("NON_PAGATA"));
+    }
+}
