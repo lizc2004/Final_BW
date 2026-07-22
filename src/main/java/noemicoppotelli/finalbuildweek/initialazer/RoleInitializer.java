013eb7209430419ffeca1dd9556da7e72f907152
cafagnarob <cafagna.rob@gmail.com>
2026-07-22 15:14:31 +0200

    Create RoleInitializer.java

---
+++ src/main/java/noemicoppotelli/finalbuildweek/initialazer/RoleInitializer.java
@@ -0,0 +1,24 @@
+package noemicoppotelli.finalbuildweek.initialazer;
+
+import noemicoppotelli.finalbuildweek.entities.Ruolo;
+import noemicoppotelli.finalbuildweek.repositories.RuoloRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Order(3)
+public class RoleInitializer implements CommandLineRunner {
+    private final RuoloRepository ruoloRepository;
+
+    public RoleInitializer(RuoloRepository ruoloRepository) {
+        this.ruoloRepository = ruoloRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.ruoloRepository.save(new Ruolo("ROLE_USER"));
+        this.ruoloRepository.save(new Ruolo("ROLE_ADMIN"));
+    }
+}
