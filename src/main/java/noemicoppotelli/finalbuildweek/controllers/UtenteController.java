f743d2e75a19cb2ef3302dd66c4d8ba8a93fde43
cafagnarob <cafagna.rob@gmail.com>
2026-07-22 17:27:58 +0200

    Create UtenteController.java

---
+++ src/main/java/noemicoppotelli/finalbuildweek/controllers/UtenteController.java
@@ -0,0 +1,107 @@
+package noemicoppotelli.finalbuildweek.controllers;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import noemicoppotelli.finalbuildweek.entities.Utente;
+import noemicoppotelli.finalbuildweek.payloads.PasswordChangeDTO;
+import noemicoppotelli.finalbuildweek.payloads.UtenteResponseDTO;
+import noemicoppotelli.finalbuildweek.payloads.UtenteUpdateDTO;
+import noemicoppotelli.finalbuildweek.service.UtenteService;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/utenti")
+@RequiredArgsConstructor
+public class UtenteController {
+    private final UtenteService utenteService;
+
+
+    //GET /utenti/{id}
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public UtenteResponseDTO findById(
+            @PathVariable Long id
+    ) {
+        Utente found = utenteService.getById(id);
+        return new UtenteResponseDTO(found.getId(), found.getUsername(),
+                found.getEmail(), found.getNome(), found.getCognome(),
+                found.getAvatar(), found.getRuoli());
+    }
+
+
+    // PUT /utenti/me
+
+    @PutMapping("/me")
+    public Utente updateMe(
+            @AuthenticationPrincipal Utente utente,
+            @Valid @RequestBody UtenteUpdateDTO body
+    ) throws BadRequestException {
+        return this.utenteService.update(utente.getId(), body);
+    }
+
+    // PUT /utenti/me/password
+    @PutMapping("/me/password")
+    @PreAuthorize("isAuthenticated()")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void changePassword(
+            @AuthenticationPrincipal Utente utente,
+            @Valid @RequestBody PasswordChangeDTO body
+    ) throws BadRequestException {
+
+        utenteService.changePassword(
+                utente.getId(),
+                body
+        );
+    }
+
+    // DELETE /utenti/{id}
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(
+            @PathVariable Long id
+    ) {
+
+        utenteService.delete(id);
+
+    }
+
+    // POST /utenti/{utenteId}/ruoli/{ruoloId}
+    @PostMapping("/{utenteId}/ruoli/{ruoloId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public Utente addRole(
+            @PathVariable Long utenteId,
+            @PathVariable Long ruoloId
+    ) throws BadRequestException {
+
+        return utenteService.addRole(
+                utenteId,
+                ruoloId
+        );
+
+    }
+
+    //DELETE /utenti/{utenteId}/ruoli/{ruoloId}
+    @DeleteMapping("/{utenteId}/ruoli/{ruoloId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public Utente removeRole(
+            @PathVariable Long utenteId,
+            @PathVariable Long ruoloId
+    ) throws BadRequestException {
+
+        return utenteService.removeRole(utenteId, ruoloId);
+    }
+
+
+    @GetMapping("/me")
+    @PreAuthorize("isAuthenticated()")
+    public Utente getMyProfile(
+            @AuthenticationPrincipal Utente utente
+    ) {
+        return utente;
+    }
+}
